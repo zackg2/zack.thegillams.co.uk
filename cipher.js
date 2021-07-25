d771f2a3a2030bac405b3e2ce48bedaf703f7536
zackg2 <50543862+zackg2@users.noreply.github.com>
2021-07-25 14:25:46 +0100

    SIMPLIFY

--- cipher.js
+++ cipher.js
@@ -1,18 +1,16 @@
 console.log("hello world");
 const mesage = "JRYY/QBAR/LBH/PENPXRQ/VG";
-const letters = mesage.split("");
-console.log(letters);
+console.log(mesage);
 const alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".repeat(3);
-const alfabetletters = alfabet.split("");
-console.log(alfabetletters);
+console.log(alfabet);
 
 let output = "";
-for (let i = 0; i < letters.length; i = i + 1) {
-  const l = letters[i];
-  const n = alfabetletters.indexOf(l);
+for (let i = 0; i < mesage.length; i = i + 1) {
+  const l = mesage[i];
+  const n = alfabet.indexOf(l);
   if (n >= 0) {
     const c = n + 13;
-    output = output + alfabetletters[c];
+    output = output + alfabet[c];
   } else {
     output = output + l;
   }
