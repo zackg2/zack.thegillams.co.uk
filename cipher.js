7cd0c21109b9ee3a0178ed9cb8a39699bdc5313f
zackg2 <50543862+zackg2@users.noreply.github.com>
2021-07-25 14:50:34 +0100

    MESAGE

--- cipher.js
+++ cipher.js
@@ -1,18 +1,24 @@
 console.log("hello world");
-const mesage = "JRYY/QBAR/LBH/PENPXRQ/VG";
+const mesage = "JUB JNAGF CEVATYRF";
 console.log(mesage);
 const alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".repeat(3);
 console.log(alfabet);
 
-let output = "";
-for (let i = 0; i < mesage.length; i = i + 1) {
-  const l = mesage[i];
-  const n = alfabet.indexOf(l);
-  if (n >= 0) {
-    const c = n + 13;
-    output = output + alfabet[c];
-  } else {
-    output = output + l;
+function decipher(mesage, rot) {
+  let output = "";
+  for (let i = 0; i < mesage.length; i = i + 1) {
+    const l = mesage[i];
+    const n = alfabet.indexOf(l);
+    if (n >= 0) {
+      const c = n + rot;
+      output = output + alfabet[c];
+    } else {
+      output = output + l;
+    }
   }
+  console.log(output);
 }
-console.log(output);
+
+for (let j = 0; j < 26; j = j + 1) {
+  decipher(mesage, j);
+}
