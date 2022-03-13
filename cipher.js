1b068e70cbd3b5e316e8c2b6e63cf7896f2be41d
zackg2 <50543862+zackg2@users.noreply.github.com>
2022-03-13 09:42:02 +0000

    more flexable cipher

--- cipher.js
+++ cipher.js
@@ -1,16 +1,16 @@
 console.log("hello world");
-const mesage = "JUB JNAGF CEVATYRF";
+const mesage = "ED DEN ZXZCNPYZ FA PKNNZN";
 console.log(mesage);
 const alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".repeat(3);
 console.log(alfabet);
 
-function decipher(mesage, rot) {
+function decipher(mesage, callback) {
   let output = "";
   for (let i = 0; i < mesage.length; i = i + 1) {
     const l = mesage[i];
     const n = alfabet.indexOf(l);
     if (n >= 0) {
-      const c = n + rot;
+      const c = callback(n);
       output = output + alfabet[c];
     } else {
       output = output + l;
@@ -20,5 +20,7 @@ function decipher(mesage, rot) {
 }
 
 for (let j = 0; j < 26; j = j + 1) {
-  decipher(mesage, j);
+  decipher(mesage, function (n) {
+    return -n + 26 + j;
+  });
 }
