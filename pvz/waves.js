579de50d01ca0c314dcf610e984a4152b80dd64b
zackg2 <50543862+zackg2@users.noreply.github.com>
2026-04-25 11:17:01 +0100

    adding mohawk to waves

--- pvz/waves.js
+++ pvz/waves.js
@@ -2,6 +2,7 @@
 
 import { Hat } from "./Hat.js";
 import { Zombie } from "./zombie.js";
+import { mohawk } from "./mohawk.js";
 
 export const WAVES = [
   {
@@ -18,6 +19,7 @@ export const WAVES = [
         zombies: [
           { klass: Zombie, count: 12 },
           { klass: Hat, count: 1 },
+          { klass: mohawk, count: 3 },
         ],
       },
     ],
@@ -29,6 +31,7 @@ export const WAVES = [
         zombies: [
           { klass: Zombie, count: 21 },
           { klass: Hat, count: 5 },
+          { klass: mohawk, count: 10 },
         ],
       },
       {
@@ -36,6 +39,7 @@ export const WAVES = [
         zombies: [
           { klass: Zombie, count: 6 },
           { klass: Hat, count: 5 },
+          { klass: mohawk, count: 6 },
         ],
       },
     ],
